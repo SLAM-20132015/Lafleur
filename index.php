b8d550cf18cc619a2488837a7e05b07067300b95
GiraudSophie <sophie.giraud@gmail.com>
2014-04-08 09:51:29 +0200

    ajout des images

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-	echo "test Lafleur";
-?>
+<html>
+<head>
+<title>menu</title>
+</head>
+<body bgcolor="#ffffff">
+<h3 align="center">"Dites-le avec Lafleur"</h3>
+<p align="center"><img src="images/lafleur.gif" /></p>
+</body>
+</html>
